0b25ea3baf0c013cb094dec340060da393b12b4c
groove4life <groove4life.7@gmail.com>
2022-06-21 03:52:06 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,20 @@
+// Falsy
+
+// false
+// 0
+// ""
+// null
+// NaN
+// undefined
+
+// Truthy
+
+// '0', ' ', []
+
+let name = 12;
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is true");
+}
