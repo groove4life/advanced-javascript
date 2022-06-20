23bc36125f43380cbbde8ff1406e85a950936def
groove4life <groove4life.7@gmail.com>
2022-06-21 04:13:14 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,20 @@
+let bird;
+function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+
+function add(num1, num2){
+    console.log(num2)
+}
+const result = add(13, 82)
+console.log(result);
+
+const boy = {name:"smart dude", phone:1234567};
+console.log(boy.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9];
+console.log(ages[11]);
